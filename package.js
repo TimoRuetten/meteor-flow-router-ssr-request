82a7c50a638807fe4568ce1c3925e707e28e4c22
Timo Rütten <timoruetten@Timos-MBP.localdomain>
2016-05-05 12:12:30 +0200

    Edited package js

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   name: 'timoruetten:flow-router-ssr-request',
-  version: '0.0.3',
+  version: '0.1.0',
   summary: 'Simply extension for FlowRouter SSR to make it possible to access the last request object',
   git: '',
   documentation: 'README.md'
@@ -10,8 +10,8 @@ Package.onUse(function(api) {
   api.versionsFrom('1.3.2.4');
   api.use('ecmascript');
   api.use([
-    'meteorhacks:picker',
-    'kadira:flow-router-ssr'
+    'meteorhacks:picker@1.0.4',
+    'kadira:flow-router-ssr@3.13.0'
   ]);
   api.addFiles('flow-router-ssr-request.js', ['server']);
 });
